4fe03fee88fd7b57a6c44715edf070c188f424b7
Sanketha_Kumbar <sankethkumbar@gmail.com>
2026-03-17 11:26:18 +0530

    Add frontend with public folder and integrate feedback API with UI

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+//app.use(express.static('public'));
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log('Server running at http://localhost:3000');
+});
